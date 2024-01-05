cfcf09f946f4203e399766411edc219f294e3b73
Roman <grndev@mail.ru>
2024-01-06 00:14:41 +0300

    Added create and update actions

---
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -0,0 +1,51 @@
+package ru.job4j.accidents.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.service.AccidentService;
+
+import java.util.Optional;
+
+@Controller
+@AllArgsConstructor
+public class AccidentController {
+    private final AccidentService accidentService;
+
+    @GetMapping("/create")
+    public String viewCreateAccident() {
+        return "accidents/create";
+    }
+
+    @PostMapping("/create")
+    public String save(@ModelAttribute Accident accident) {
+        accidentService.save(accident);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable int id) {
+        Optional<Accident> accidentOptional = accidentService.findById(id);
+        if (accidentOptional.isEmpty()) {
+            model.addAttribute("message", "Accident with this id is not found");
+            return "errors/404";
+        }
+        model.addAttribute("accident", accidentOptional.get());
+        return "accidents/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Accident accident, Model model) {
+        var isUpdated = accidentService.update(accident);
+        if (!isUpdated) {
+            model.addAttribute("message", "Accident with this id is not found");
+            return "errors/404";
+        }
+        return "redirect:/";
+    }
+}
